6f7f165f5066ceab9e19a950976b7891c796710a
mart22052006 <157504417+Mart22052006@users.noreply.github.com>
2025-01-17 18:42:16 +0300

    Update urls.py

--- server/webapp/urls.py
+++ server/webapp/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-
-from webapp.views import *
+from webapp import views
 
 urlpatterns = [
-    path('index', index),
-    path('login', login),
-    path('register', register),
-    path('get_user', get_user),
-    path('update_user', update_user),
-    path('get_movies', get_movies),
-    path('all',all),
-    path('top', top),
+    path('index', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('get_user', views.get_user, name='get_user'),
+    path('update_user', views.update_user, name='update_user'),
+    path('get_movies', views.get_movies, name='get_movies'),
+    path('all', views.all, name='all_movies'),
+    path('top', views.top, name='top_movies'),
+    path('calendar-events', views.get_google_calendar_events, name='calendar_events'),
+    path('tasks', views.get_tasks, name='get_tasks'),
+    path('tasks/create', views.create_task, name='create_task'),
+    path('', views.index, name='home')
 ]
